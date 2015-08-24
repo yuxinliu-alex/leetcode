9a8936c5b0c3abc1d0ba898bb15d4776070efb59
Alex Liu <yuxinliu.alex@gmail.com>
2015-08-24 15:01:47 +0800

    first time

---
+++ LeetCode/LeetCode/Poj10.h
@@ -0,0 +1,13 @@
+//
+//  Poj10.h
+//  LeetCode
+//
+//  Created by Alex on 15/8/24.
+//  Copyright (c) 2015年 Alex. All rights reserved.
+//
+
+#ifndef LeetCode_Poj10_h
+#define LeetCode_Poj10_h
+
+
+#endif
---
+++ LeetCode/LeetCode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LeetCode
+//
+//  Created by Alex on 15/8/24.
+//  Copyright (c) 2015年 Alex. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Poj10.h"
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
