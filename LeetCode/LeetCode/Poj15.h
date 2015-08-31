cc15ee7bd36c7bf6e8b042b18ef6dd1e4b6e6db8
Alex Liu <yuxinliu.alex@gmail.com>
2015-08-31 17:16:08 +0800

    finally

--- LeetCode/LeetCode/Poj15.h
+++ LeetCode/LeetCode/Poj15.h
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#define MAX 2048
 /**
  * Return an array of arrays of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
@@ -31,35 +32,104 @@ void quickSort(int* s,int l,int r){
         quickSort(s,i+1,r);
     }
 }
-
-
-void threeSum(int* nums, int numsSize, int* returnSize) {
+int** threeSum(int* nums, int numsSize, int* returnSize) {
     int p_count = 0;
     int n_count = 0;
     bool nozero = false;
-    quickSort(nums,0,numsSize);
+    int** sum = (int**)calloc(MAX, sizeof(int*));
+    int size = 0;
+    quickSort(nums,0,numsSize-1);
     for (int i=0; i<numsSize; ++i) {
-        if (n_count==0&&nums[i]>=0)
-            n_count=i;
-        if (p_count==0&&nums[i]>0) {
-            p_count=i;
+        if(nums[i]<0)
+            n_count++;
+        if (nums[i]>0) {
+            p_count++;
         }
     }
-    if (n_count==p_count)
+    if (n_count+p_count==numsSize)
         nozero=true;
-    if (n_count==0||p_count==0) {
-        return ;
+    if (numsSize-(n_count+p_count)>2) {
+        sum[size] = (int*)malloc(3*sizeof(int));
+        sum[size][0] = 0;
+        sum[size][1] =  0;
+        sum[size++][2] = 0;
     }
-    p_count=numsSize-p_count;
+    if (n_count==0||p_count==0){
+        if (size==0)
+            return NULL;
+        else{
+            *returnSize=size;
+            return sum;}
+    }
     int negtive_nums[n_count];
     int positive_nums[p_count];
-    for (int i = 0; i<n_count; ++i ) {
-        negtive_nums[i] = nums[i];
-        printf("%d   ",negtive_nums[i]);
+    for (int i = 0; i<n_count; ++i )
+        negtive_nums[n_count-i-1] = -nums[i];
+    for (int i=numsSize-p_count,k=0; i<numsSize; ++i,++k)
+        positive_nums[k] = nums[i];
+    int i = 0;
+    int j = 0;
+    while (i<n_count) {
+        if (i>0) {
+            if (negtive_nums[i]==negtive_nums[i-1]) {
+                ++i;
+                continue;
+            }
+        }
+        for (int k=0; k<p_count; ++k) {
+            if (positive_nums[k]<negtive_nums[i]) {
+                for (int t=k+1; t<p_count; ++t) {
+                    if(positive_nums[t]+positive_nums[k]>negtive_nums[i])
+                        break;
+                    else if(positive_nums[t]+positive_nums[k]==negtive_nums[i]){
+                        sum[size] = (int*)malloc(3*sizeof(int));
+                        sum[size][0] = -negtive_nums[i];
+                        sum[size][1] =  positive_nums[k];
+                        sum[size++][2] = positive_nums[t];
+                        break;
+                    }
+                }
+                while (k<p_count&&positive_nums[k+1]==positive_nums[k]) k++;
+            }
+            else if (!nozero&&positive_nums[k]==negtive_nums[i]){
+                sum[size] = (int*)malloc(3*sizeof(int));
+                sum[size][0] = -negtive_nums[i];
+                sum[size][1] =  0;
+                sum[size++][2] = positive_nums[k];
+                break;
+            }
+            else
+                break;
+        }
+        ++i;
     }
-    for (int i=numsSize-p_count; i<numsSize; ++i)
-        positive_nums[i] = nums[i];
-    //free(nums);
-    free(returnSize);
-    //return &nums;
+    while (j<p_count) {
+        if (j>0)
+            if (positive_nums[j]==positive_nums[j-1]) {
+                j++;
+                continue;
+            }
+        for (int k = 0; k<n_count; ++k) {
+            if (negtive_nums[k]<positive_nums[j]) {
+                for (int t=k+1; t<n_count; ++t) {
+                    if(negtive_nums[t]+negtive_nums[k]>positive_nums[j])
+                        break;
+                    else if (negtive_nums[t]+negtive_nums[k]==positive_nums[j]){
+                        sum[size] = (int*)malloc(3*sizeof(int));
+                        sum[size][0] = -negtive_nums[t];
+                        sum[size][1] = -negtive_nums[k];
+                        sum[size++][2] = positive_nums[j];
+                        break;
+                    }
+                }
+                while (k<n_count&&negtive_nums[k+1]==negtive_nums[k]) k++;
+            }
+            else
+                break;
+            
+        }
+        ++j;
+    }
+    *returnSize = size;
+    return sum;
 }
--- LeetCode/LeetCode/main.c
+++ LeetCode/LeetCode/main.c
@@ -9,8 +9,14 @@
 #include <stdio.h>
 #include "Poj15.h"
 int main(int argc, const char * argv[]) {
-    int s[]={-1,0,1,2,-1,-4};
-    int* p;
-    threeSum(s, 6, p);
+    int s[]={-1,0,1};
+    int num = 0;
+    int* p =&num;
+    int** a;
+    a = threeSum(s, 3, p);
+    for (int i =0; i<num; ++i) {
+        printf("%d,%d,%d\n",a[i][0],a[i][1],a[i][2]);
+    }
     return 0;
+    
 }
